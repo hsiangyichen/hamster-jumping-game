e1760eea3db9c70670cca2182bcdeeb1ee8366fb
hsiangyichen <hsiangyi1025@gmail.com>
2024-07-08 23:14:28 -0700

    fix: cross-area

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,28 @@
-// next.config.mjs
-import withSVGR from "@svgr/webpack";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        and: [/\.(js|ts)x?$/],
-      },
       use: ["@svgr/webpack"],
     });
-
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN", // or 'ALLOW-FROM https://example.com'
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://example.com", // Update with your requirements
+          },
+        ],
+      },
+    ];
   },
 };
 
