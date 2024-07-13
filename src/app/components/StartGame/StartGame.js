adf56148bba748de468b9a258d6f924ecc182108
hsiangyichen <hsiangyi1025@gmail.com>
2024-07-13 03:08:15 -0700

    feat: add more instructions and space key

--- src/app/components/StartGame/StartGame.js
+++ src/app/components/StartGame/StartGame.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 
 const StartGame = ({ onStart }) => {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsAnimating((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.instruction}>Jump to avoid the cats and wheels!</h1>
+      <div className={styles.seedImageContainer}>
+        <h1 className={styles.instructionOne}>Find the seeds!</h1>
+        <Image
+          src="/images/seed.png"
+          alt="seed"
+          width={650}
+          height={650}
+          className={styles.seedImage}
+        />
+      </div>
+
+      <div className={styles.spaceImageContainer}>
+        <h1 className={styles.instructionTwo}>
+          Jump to avoid the cats and wheels!
+        </h1>
+        <div className={styles.spaceAndArrowImageContainer}>
+          <Image
+            src="/images/space.png"
+            alt="seed"
+            width={650}
+            height={650}
+            className={styles.spaceImage}
+          />
+          <Image
+            src="/images/arrow.png"
+            alt="arrow"
+            width={650}
+            height={650}
+            className={`${styles.arrowImage} ${
+              isAnimating ? styles.animate : ""
+            }`}
+          />
+        </div>
+      </div>
+
       <button onClick={onStart} className={styles.startButton}>
         <Image
           src="/images/startButton.png"
           width={175}
           height={55}
           alt="startButton"
+          className={styles.startButtonImage}
         />
       </button>
     </div>
